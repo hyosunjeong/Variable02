70408d6fdd4e5125d80e1c75f8381ba3977c8f3c
hyosunjeong <jeong8161@naver.com>
2018-11-14 16:33:32 +0900

    first

---
+++ src/com/biz/var/VariableType03.java
@@ -0,0 +1,33 @@
+package com.biz.var;
+
+public class VariableType03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// boolean은 비교 연산자를 통해 나온 결과
+		int intNum1 = 30;
+		int intNum2 = 40;
+		
+		boolean bNum;
+		
+		bNum = intNum1 < intNum2;
+		System.out.println(bNum);
+		
+		bNum = intNum1 > intNum2;
+		System.out.println(bNum);
+		
+		bNum = intNum1 == intNum2;
+		System.out.println(bNum);
+		
+		bNum = (intNum1 <= intNum2);
+		System.out.println(bNum);
+		
+		bNum = intNum1 >= intNum2;
+		System.out.println(bNum);
+		
+		
+		
+	}
+
+}
